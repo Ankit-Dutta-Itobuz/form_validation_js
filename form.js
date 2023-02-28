4fe834eb998d7abf2a8d90c608a7ff2653e7ebd8
dutta_ankit <ankit.dutta@itobuz.com>
2023-02-28 18:25:16 +0530

    first commit

---
+++ form.js
@@ -0,0 +1,11 @@
+function validate(){
+    var name = document.form.name.value;
+    var password = document.form.password.value;
+    var email = document.form.email.value;
+    
+    if(name==null || name=="")
+    {
+      alert("ENTER A VALID NAME");
+      return false;
+    }
+    }
